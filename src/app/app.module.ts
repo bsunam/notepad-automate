300eef01dc668b9b4a737634d95a7b86b5fdc329
qxx0245 <sunamb@achalasolutions.com>
2020-07-26 21:27:17 +0530

    Added required redux dependencies and required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './shared/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { DemoMaterialModule } from './shared/modules/material.module';
+import { HeaderContentComponent } from './header-content/header-content.component';
+import { SidebarContentComponent } from './sidebar-content/sidebar-content.component';
+import { NoteContentComponent } from './note-content/note-content.component';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderContentComponent,
+    SidebarContentComponent,
+    NoteContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/note-content/note-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-content',
+  templateUrl: './note-content.component.html',
+  styleUrls: ['./note-content.component.scss']
+})
+export class NoteContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
